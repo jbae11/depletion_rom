0adc1f6fb1d71f0f50d5de3cf0cdfff76aed726e
jbae11 <jbae11@illinois.edu>
2018-10-14 20:56:08 -0500

    fix error

--- curate_db.py
+++ curate_db.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 def get_assem_comp_dict(assem_df):
     comp_dict = {}
@@ -23,20 +24,20 @@ print(n_assem)
 one_percent = n_assem // 100
 print('One Percent')
 print(one_percent)
-new_dict = {}
+new_dict = OrderedDict({})
 i = 0 
 for assem in assembly_ids:
     assem_df = df.loc[df['assembly_id'] == assem]
-
-    assem_dict = get_assem_comp_dict(assem_df)
     assem_dict['reactor_id'] = get_first_from_column(assem_df, 'reactor_id')
     assem_dict['reactor_type'] = get_first_from_column(assem_df, 'reactor_type')
     assem_dict['init_enr'] = get_first_from_column(assem_df, 'initial_enrichment')
     assem_dict['bu'] = get_first_from_column(assem_df, 'discharge_burnup')
+    assem_dict = get_assem_comp_dict(assem_df)
+
     new_dict[assem] = assem_dict
     if i%one_percent == 0:
-        print('%i %% Done' %int(n_assem / one_percent))
+        print('%i %% Done' %int(i / one_percent))
     i += 1
 
 new_df = pd.DataFrame.from_dict(new_dict, orient='index')
-pd.DataFrame.to_csv('./curated.csv')
+new_df.to_csv('./curated.csv')
