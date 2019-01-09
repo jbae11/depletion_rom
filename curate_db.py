3fad68b4374692cc8805460b6e2f33e848b53c7d
Jin Whan Bae <jbae11@illinois.edu>
2019-01-09 14:26:09 -0600

    please include init enr and bu

--- curate_db.py
+++ curate_db.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
-def get_assem_comp_dict(assem_df):
+def get_assem_comp_dict(assem_dict, assem_df):
     comp_dict = {}
     for index, row in assem_df.iterrows():
         comp_dict[row['name']] = row['total_mass_g']
     total_mass_assem = sum(comp_dict.values())
     for key, val in comp_dict.items():
-        comp_dict[key] = val / total_mass_assem
-    return comp_dict
+        assem_dict[key] = val / total_mass_assem
+    return assem_dict
 
 def get_first_from_column(df, column_name):
     return np.array(df[column_name])[0]
@@ -27,13 +27,12 @@ print(one_percent)
 new_dict = OrderedDict({})
 i = 0 
 for assem in assembly_ids:
-    assem_dict = {}
     assem_df = df.loc[df['assembly_id'] == assem]
-    assem_dict['reactor_id'] = get_first_from_column(assem_df, 'reactor_id')
+    assem_dict = {}
     assem_dict['reactor_type'] = get_first_from_column(assem_df, 'reactor_type')
     assem_dict['init_enr'] = get_first_from_column(assem_df, 'initial_enrichment')
     assem_dict['bu'] = get_first_from_column(assem_df, 'discharge_burnup')
-    assem_dict = get_assem_comp_dict(assem_df)
+    assem_dict = get_assem_comp_dict(assem_dict, assem_df)
     new_dict[assem] = assem_dict
     if i%one_percent == 0:
         print('%i %% Done' %int(i / one_percent))
