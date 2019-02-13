1a00e4bf6fc444c8ca09785b676a77a5dbf67b27
Jin Whan Bae <jbae11@illinois.edu>
2019-02-13 12:17:38 -0500

    user-defined filepath

--- script/curate_db.py
+++ script/curate_db.py
@@ -16,30 +16,36 @@ def get_assem_comp_dict(assem_dict, assem_df, assem_mass_kg):
 def get_first_from_column(df, column_name):
     return np.array(df[column_name])[0]
 
-df = pd.read_csv('../db/udb_1yr.dat', sep='\t')
 
-assembly_ids = np.unique(df['assembly_id'])
-n_assem = len(assembly_ids)
-print('Number of assemblies')
-print(n_assem)
-one_percent = n_assem // 100
-print('One Percent')
-print(one_percent)
-new_dict = OrderedDict({})
-i = 0
-for assem in assembly_ids:
-    assem_df = df.loc[df['assembly_id'] == assem]
-    assem_dict = {}
-    assem_dict['reactor_type'] = get_first_from_column(assem_df, 'reactor_type')
-    assem_dict['total_mass'] = get_first_from_column(assem_df, 'initial_uranium_kg')
-    assem_dict['evaluation_date'] = get_first_from_column(assem_df, 'evaluation_date')
-    assem_dict['init_enr'] = get_first_from_column(assem_df, 'initial_enrichment')
-    assem_dict['bu'] = get_first_from_column(assem_df, 'discharge_burnup')
-    assem_dict = get_assem_comp_dict(assem_dict, assem_df, assem_dict['total_mass'])
-    new_dict[assem] = assem_dict
-    if i%one_percent == 0:
-        print('%i %% Done' %int(i / one_percent))
-    i += 1
+def main(filepath):
+    df = pd.read_csv(filepath, sep='\t')
 
-new_df = pd.DataFrame.from_dict(new_dict, orient='index')
-new_df.to_csv('./curated.csv')
+    assembly_ids = np.unique(df['assembly_id'])
+    n_assem = len(assembly_ids)
+    print('Number of assemblies')
+    print(n_assem)
+    one_percent = n_assem // 100
+    print('One Percent')
+    print(one_percent)
+    new_dict = OrderedDict({})
+    i = 0
+    for assem in assembly_ids:
+        assem_df = df.loc[df['assembly_id'] == assem]
+        assem_dict = {}
+        assem_dict['reactor_type'] = get_first_from_column(assem_df, 'reactor_type')
+        assem_dict['total_mass'] = get_first_from_column(assem_df, 'initial_uranium_kg')
+        assem_dict['evaluation_date'] = get_first_from_column(assem_df, 'evaluation_date')
+        assem_dict['init_enr'] = get_first_from_column(assem_df, 'initial_enrichment')
+        assem_dict['bu'] = get_first_from_column(assem_df, 'discharge_burnup')
+        assem_dict = get_assem_comp_dict(assem_dict, assem_df, assem_dict['total_mass'])
+        new_dict[assem] = assem_dict
+        if i%one_percent == 0:
+            print('%i %% Done' %int(i / one_percent))
+        i += 1
+
+    new_df = pd.DataFrame.from_dict(new_dict, orient='index')
+    new_df.to_csv('./curated.csv')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
